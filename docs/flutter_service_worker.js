a95a54df3aeca3e1bb7281cf431664840547e671
mohammadsofy <mohammadsofyan079@gmail.com>
2025-10-28 12:15:32 +0300

    updated menu

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -3,7 +3,8 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"assets/AssetManifest.bin": "d86bd6c0d4d9dd29996190a2219efe6c",
+const RESOURCES = {"404.html": "0c4e47becb20a2454b760dce2f91e716",
+"assets/AssetManifest.bin": "d86bd6c0d4d9dd29996190a2219efe6c",
 "assets/AssetManifest.bin.json": "3c4945d4c54a3806dbae1693f60e1cbc",
 "assets/AssetManifest.json": "ca369c40f6cbae6e0180b5483e99ab69",
 "assets/FontManifest.json": "dc3d03800ccca4601324923c0b1d6d57",
@@ -27,7 +28,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "d86bd6c0d4d9dd29996190a2219efe6c
 "canvaskit/skwasm_heavy.wasm": "8034ad26ba2485dab2fd49bdd786837b",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
-"flutter_bootstrap.js": "e6d781db1376a82283e3e17b2c1c19db",
+"flutter_bootstrap.js": "92731eaaa7c2420a1300abdf036860ab",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
